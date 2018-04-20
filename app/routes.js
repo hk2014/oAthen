caead6601afb08d442ff32233ac92974ae5d9e78
shawnhr <shawnk0818@gmail.com>
2018-04-20 09:21:58 -0700

    Add passport authentication for login

--- app/routes.js
+++ app/routes.js
@@ -34,7 +34,12 @@ module.exports = function (app, passport) {
     // app.post('/signup', do all our passport stuff here);
     // app/routes.js
 
-
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/profile', // redirect to the secure profile section
