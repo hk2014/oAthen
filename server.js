481674896bee995a968a88ac6148e9b3768cad91
shawnhr <shawnk0818@gmail.com>
2018-04-20 02:47:49 -0700

    Adds login page and welcome page

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 
 app.use(bodyParser()); // get information from html forms
-
+app.set('views', './app/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
